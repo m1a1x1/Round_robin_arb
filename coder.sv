a44f96462fbf962d34a091dec368fd1e7ec897aa
m1a1x1 <gsuperpromax@gmail.com>
2014-11-12 01:53:26 +0300

    Update coder.sv
    Problem fixed

--- coder.sv
+++ coder.sv
@@ -6,15 +6,18 @@ module priority_coder
 )
 
 (
-  input        [REQCNT-1:0]        data_i,
-  input        [REQWIDTH-1:0]      prior_i,
-  output logic [REQWIDTH-1:0]      data_num_o
+  input                       clk_i,
+  input                       rst_i,
+
+  input        [REQCNT-1:0]   data_i,
+  input        [REQWIDTH-1:0] prior_i,
+  output logic [REQWIDTH-1:0] data_num_o
 );
 
 always_comb
   begin
     data_num_o = 0;
-      if( data_i[ prior_i ] == 1 )
+      if( data_i[ prior_i ] )
         begin
           data_num_o = prior_i;
         end
@@ -22,10 +25,13 @@ always_comb
         begin
           for( int i = 0;  i < REQCNT; i++ ) 
             begin	 
-              if( data_i[ i ] )		
-                data_num_o = i;	
+              if( data_i[ i ] )
+                begin    
+                  data_num_o = i;
+                  i = REQCNT;  
+                end
             end
         end 
   end
 
-endmodul
+endmodule
