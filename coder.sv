d18075f19034814ae39dcf601a799c4a0dbf021c
m1a1x1 <gsuperpromax@gmail.com>
2014-11-12 23:16:24 +0300

    Update coder.sv

--- coder.sv
+++ coder.sv
@@ -9,29 +9,97 @@ module priority_coder
   input                       clk_i,
   input                       rst_i,
 
-  input        [REQCNT-1:0]   data_i,
-  input        [REQWIDTH-1:0] prior_i,
+  input logic  [REQCNT-1:0]   data_i,
+  input logic  [REQWIDTH-1:0] prior_i,
   output logic [REQWIDTH-1:0] data_num_o
 );
+
+logic [REQCNT-1:0] req_high;
+logic [REQCNT-1:0] req_low;
+
+logic [REQWIDTH-1:0] hight_num;
+logic [REQWIDTH-1:0] low_num;
+
+logic [REQCNT-1:0] mask_hight;
+logic [REQCNT-1:0] mask_low;
+
+assign req_high = data_i & mask_hight;
+assign req_low  = data_i & mask_low;
+
+always_comb
+  begin
+    for( int i = 0; i < REQCNT; i++ )
+      begin
+        if( i < prior_i )
+          begin
+             mask_hight[ i ] = 0;
+             mask_low[ i ]   = 1;
+          end
+        else
+          begin
+            if( i == prior_i )
+              begin
+                mask_hight[ i ] = 0;
+                mask_low[ i ]   = 0;
+              end
+            else
+              begin
+                mask_hight[ i ] = 1;
+                mask_low[ i ]   = 0;
+              end
+          end
+      end
+  end
+
+always_comb
+  begin
+    for( int i = 0; i < REQCNT; i++ )
+      begin
+        if( req_high[ i ] )
+          begin
+            hight_num = i;
+            i = REQCNT;
+          end
+        else
+          begin
+            hight_num = 0;
+          end
+      end
+  end
+
+always_comb
+  begin
+    for( int i = 0; i < REQCNT; i++ )
+      begin
+        if( req_low[ i ] )
+          begin
+            low_num = i;
+            i = REQCNT;
+          end
+        else
+          begin
+            low_num = 0;
+          end
+      end
+  end
 
 always_comb
   begin
     data_num_o = 0;
-      if( data_i[ prior_i ] )
-        begin
-          data_num_o = prior_i;
-        end
-      else
-        begin
-          for( int i = 0;  i < REQCNT; i++ ) 
-            begin	 
-              if( data_i[ i ] )
-                begin    
-                  data_num_o = i;
-                  i = REQCNT;  
-                end
-            end
-        end 
+    if( data_i[ prior_i ] )
+      begin
+        data_num_o = prior_i;
+      end
+    else
+      begin
+        if( hight_num != 0 )
+          begin
+            data_num_o = hight_num + prior_i;
+          end
+        else
+          begin
+            data_num_o = low_num;
+          end
+      end
   end
-
 endmodule
